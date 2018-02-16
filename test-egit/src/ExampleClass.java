d044ec4a528cea15ba8ba7e47acdb6f84f627366
Rafael Silva <mind_traveller@hotmail.com>
2018-02-16 15:03:48 +0100

    Created a new class and a method within it

---
+++ test-egit/src/ExampleClass.java
@@ -0,0 +1,7 @@
+
+public class ExampleClass {
+	public void myMethod1() {
+		System.out.println("Invoking method1");
+	}
+
+}
--- test-egit/src/HelloWorld.java
+++ test-egit/src/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("This line was written in rafael-wip");
+		
+		ExampleClass ec = new ExampleClass();
+		ec.myMethod1();
 
 	}
 
